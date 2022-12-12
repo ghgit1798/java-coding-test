51d805c041da1cc374050822d1d26a8e76d0b5f2
ghgit1798 <ghgoo1798@gmail.com>
2022-12-12 19:56:22 +0900

    chap01 기본 알고리즘

---
+++ chap01/src/TriangleLB.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class TriangleLB {
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+        int n;
+
+
+        System.out.println("왼쪽 아래가 직각인 이등변삼각형을 출력합니다.");
+
+        do {
+            System.out.print("몇 단 삼각형입니까?: ");
+            n = stdIn.nextInt();
+        } while (n<=0);
+
+        for (int i=1; i<=n; i++) {
+            System.out.println("*".repeat(i));
+        }
+    }
+}
