e162e274c6535e8c4e5959edf614ec9824571c17
ghgit1798 <ghgoo1798@gmail.com>
2022-12-30 00:15:48 +0900

    221220 형변환

--- coding-test/src/P11720_숫자의합.java
+++ coding-test/src/P11720_숫자의합.java
@@ -14,3 +14,33 @@ public class P11720_숫자의합 {
         System.out.print(sum);
     }
 }
+
+/*
+String sNum = "1234";
+int i1 = Integer.parseInt(sNum);
+int i2 = Integer.valueOf(sNum);
+double d1 = Double.parseDouble(sNum);
+double d2 = Double.valueOf(sNum);
+float f1 = Float.parseFloat(sNum);
+float f2 = Float.valueOf(sNum);
+long l1 = Long.parseLong(sNum);
+long l2 = Long.valueOf(sNum);
+short s1 = Short.parseShort(sNum);
+short s2 = Short.valueOf(sNum);
+
+int i = 1234;
+String i1 = String.valueOf(i);
+String i2 = Integer.toString(i);
+double d = 1.23;
+String d1 = String.valueOf(d);
+String d2 = Double.toString(d);
+float f = (float) 1.23;
+String f1 = String.valueOf(f);
+String f2 = Float.toString(f);
+long l = 1234;
+String l1 = String.valueOf(l);
+String l2 = Long.toString(l);
+short s = 1234;
+String s1 = String.valueOf(s);
+String s2 = Short.toString(s);
+ */
