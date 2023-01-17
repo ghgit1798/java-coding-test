d5c95e0d3db3cc6443bed6f074f8d11e33c17e59
ghgit1798 <ghgoo1798@gmail.com>
2023-01-18 00:50:02 +0900

    자연정렬과 comparator

--- chap06/src/MergeSort.java
+++ chap06/src/MergeSort.java
@@ -3,24 +3,24 @@ import java.util.Scanner;
 
 public class MergeSort {
     static int[] buff;      // 작업용 배열
-    
-    // a[left] ~ a[right]를 재귀적으로 병합 정렬
+
+    // a[left]~a[right]를 재귀적으로 병합 정렬
     static void __mergeSort(int[] a, int left, int right) {
         if(left < right) {
             int i;
-            int center = (left+right) / 2;
-            int p = 0;
-            int j = 0;
-            int k = left;
+            int center = (left+right)/2;
+            int p=0;
+            int j=0;
+            int k=left;
 
-            __mergeSort(a, left, center);
-            __mergeSort(a, center+1, right);
+            __mergeSort(a, left, center);           // 배열의 앞 부분을 병합 정렬
+            __mergeSort(a, center+1, right);        // 배열의 뒷 부분을 병합 정렬
 
             for(i = left; i<=center; i++)
                 buff[p++] = a[i];
             
-            while(i<=right && j<p)
-                a[k++] = (buff[j]<=a[i]) ? buff[j++] : a[i++];
+            while(i <= right && j < p)
+                a[k++] = (buff[j] <= a[i]) ? buff[j++] : a[i++];
             
             while(j < p)
                 a[k++] = buff[j++];
@@ -29,11 +29,11 @@ public class MergeSort {
 
     // 병합 정렬
     static void mergeSort(int[] a, int n) {
-        buff = new int[n];          // 작업용 배열을 생성
+        buff = new int[n];
 
-        __mergeSort(a, 0, n-1);     // 배열 전체를 병합 정렬
+        __mergeSort(a, 0, n-1);         // 배열 전체를 병합 정렬
 
-        buff = null;                    // 작업용 배열 해제
+        buff = null;                    // 작업용 배열을 해제
     }
 
     public static void main(String[] args) {
@@ -45,7 +45,7 @@ public class MergeSort {
         int[] x = new int[nx];
 
         for(int i=0; i<nx; i++) {
-            System.out.print("x[" + i + "]: ");
+            System.out.print("x["+i+"]: ");
             x[i] = stdIn.nextInt();
         }
 
@@ -53,6 +53,6 @@ public class MergeSort {
 
         System.out.println("오름차순으로 정렬했습니다.");
         for(int i=0; i<nx; i++)
-            System.out.println("x["+i+"]: "+x[i]);
+            System.out.println("x["+i+"] = " +x[i]);
     }
 }
